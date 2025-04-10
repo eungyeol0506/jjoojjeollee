2280494d7681f6a7a9419d6c00805e14bbc12178
eungyeol0506 <janess1@naver.com>
2025-04-10 22:58:29 +0900

    init commit

---
+++ backend/src/main/java/app/project/jjoojjeollee/JjoojjeolleeApplication.java
@@ -0,0 +1,13 @@
+package app.project.jjoojjeollee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjoojjeolleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjoojjeolleeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/project/jjoojjeollee/JjoojjeolleeApplicationTests.java
@@ -0,0 +1,13 @@
+package app.project.jjoojjeollee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjoojjeolleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
