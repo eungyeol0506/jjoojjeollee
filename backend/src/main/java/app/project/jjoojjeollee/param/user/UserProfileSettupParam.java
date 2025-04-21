99fe2c8852f96a0bade4010abf00cab85136dc14
eungyeol0506 <janess1@naver.com>
2025-04-21 23:49:49 +0900

    fix: NoArgsConstructor 추가

--- backend/src/main/java/app/project/jjoojjeollee/param/user/UserProfileSettupParam.java
+++ backend/src/main/java/app/project/jjoojjeollee/param/user/UserProfileSettupParam.java
@@ -1,10 +1,13 @@
 package app.project.jjoojjeollee.param.user;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class UserProfileSettupParam {
     private String nickname;
     private String lineMessage;
