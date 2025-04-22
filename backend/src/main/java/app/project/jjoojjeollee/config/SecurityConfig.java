f00174d924da70f02d26e5cc58be4b70fe4000e8
eungyeol0506 <janess1@naver.com>
2025-04-23 00:28:39 +0900

    fix: login/signup security 설정
    -로그인 또는 회원가입 시에는 인증/인가 전이므로 허용 설정 추가

--- backend/src/main/java/app/project/jjoojjeollee/config/SecurityConfig.java
+++ backend/src/main/java/app/project/jjoojjeollee/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package app.project.jjoojjeollee.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +17,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/signup").permitAll()
+                        .requestMatchers("/api/users/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
     }
 }
