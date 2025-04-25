98dd8ecb506b2d5483786b8a66c6eb8cd7d660e9
eungyeol0506 <janess1@naver.com>
2025-04-25 12:48:50 +0900

    chore: jwtService 추가

--- backend/src/main/java/app/project/jjoojjeollee/config/SecurityConfig.java
+++ backend/src/main/java/app/project/jjoojjeollee/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package app.project.jjoojjeollee.config;
 
+import app.project.jjoojjeollee.api.JwtTokenFilter;
+import app.project.jjoojjeollee.repository.UserRepository;
+import app.project.jjoojjeollee.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +13,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,14 +25,15 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, UserRepository userRepository, JwtService jwtService, ObjectMapper objectMapper) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/signup").permitAll()
-                        .requestMatchers("/api/users/login").permitAll()
+                        .requestMatchers("/api/users/signup", "/api/users/login").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(new JwtTokenFilter(userRepository, jwtService, objectMapper), UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
     }
---
+++ backend/src/test/java/app/project/jjoojjeollee/service/JwtServiceTest.java
@@ -0,0 +1,60 @@
+package app.project.jjoojjeollee.service;
+
+import app.project.jjoojjeollee.domain.user.User;
+import app.project.jjoojjeollee.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DisplayName("JwtService Test")
+@Transactional
+class JwtServiceTest {
+    @Autowired private JwtService jwtService;
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    @DisplayName("토큰 생성 및 parsing 성공")
+    public void createAndParseToken() throws Exception{
+        //given
+        User user = User.createUser("id","pw", "email");
+        Long userNo = userRepository.save(user);
+
+        //when
+        String token = jwtService.toToken(user);
+        Long subject = jwtService.getSubFromToken(token).orElse(0L);
+
+        //then
+        assertNotNull(token, "토큰값이 생성됨");
+        assertNotEquals(0L, subject, "subject 추출 성공");
+        assertEquals(userNo, subject, "추출된 no가 테스트 userNo 값이랑 동일");
+    }
+
+    @Test
+    @DisplayName("토큰 추출 실패하는 경우")
+    public void invalidToken() throws Exception{
+        //given
+        User user = User.createUser("id","pw", "email");
+        Long userNo = userRepository.save(user);
+        User user2 = User.createUser("id2","pw2", "email2");
+        userRepository.save(user2);
+
+        String token = jwtService.toToken(user);
+        String invalidToken = "abc.def.ght"; // 엉터리 토큰
+        String invalidToken2 = jwtService.toToken(user2); //완전 다른 토큰
+        //when
+        Long subject = jwtService.getSubFromToken(token).orElse(0L);
+        Long subject2 = jwtService.getSubFromToken(invalidToken).orElse(0L);
+        Long subject3 = jwtService.getSubFromToken(invalidToken2).orElse(0L);
+
+        //then
+        assertNotEquals(subject, subject2);
+        assertNotEquals(subject, subject3);
+        assertNotEquals(userNo, subject2);
+        assertNotEquals(userNo, subject3);
+    }
+}
