e505e063deb89adef768425d70bed26a13143acd
eungyeol0506 <janess1@naver.com>
2025-05-16 17:19:45 +0900

    test: test용 설정파일 적용

--- backend/src/test/java/app/project/jjoojjeollee/repository/UserRepositoryTest.java
+++ backend/src/test/java/app/project/jjoojjeollee/repository/UserRepositoryTest.java
@@ -9,15 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ActiveProfiles("test")
 @DataJpaTest
 @Import({UserRepository.class})
-//@Transactional
 class UserRepositoryTest {
 
     @Autowired
