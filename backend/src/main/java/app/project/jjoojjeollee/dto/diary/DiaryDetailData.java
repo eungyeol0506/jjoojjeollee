e6ffa1a1f9ea59889f252a1f4d04fc95dffde4b1
eungyeol0506 <janess1@naver.com>
2025-05-26 21:43:34 +0900

    feat: JOOQ 조회 시 필요한 DTO 추가

---
+++ backend/src/main/java/app/project/jjoojjeollee/dto/diary/DiaryDetailData.java
@@ -0,0 +1,11 @@
+package app.project.jjoojjeollee.dto.diary;
+
+import java.util.List;
+
+public class DiaryDetailData {
+
+    private Long no;
+    private String name;
+
+    private List<DiaryEntryDto> diaryEntries;
+}
