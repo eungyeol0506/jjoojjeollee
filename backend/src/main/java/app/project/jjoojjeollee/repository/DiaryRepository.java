3275d630377c789ef29ea4bb89529a410a827764
eungyeol0506 <janess1@naver.com>
2025-06-20 22:50:17 +0900

    feat: 정렬조건이 있는 select 메서드 추가

--- backend/src/main/java/app/project/jjoojjeollee/repository/DiaryRepository.java
+++ backend/src/main/java/app/project/jjoojjeollee/repository/DiaryRepository.java
@@ -1,10 +1,14 @@
 package app.project.jjoojjeollee.repository;
 
 import app.project.jjoojjeollee.domain.diary.Diary;
+import app.project.jjoojjeollee.dto.diary.DiaryListDTO;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
+import org.jooq.SortField;
 import org.springframework.stereotype.Repository;
+import target.tables.Diaries;
+import target.tables.DiaryMembers;
 
 import java.util.List;
 
@@ -55,5 +59,45 @@ public class DiaryRepository {
     /**
      * select: 일기 목록 조회 메서드
      */
+    public List<DiaryListDTO> findDiaryListByUserNo(Long userNo, DiarySortOption sortOption) {
+        SortField<?> sortField = getSortField(sortOption);
 
+        Diaries d = Diaries.DIARIES.as("d");
+        DiaryMembers dm = DiaryMembers.DIARY_MEMBERS.as("dm");
+
+        return dsl.select(
+                d.DIARY_NO,
+                d.NAME,
+                d.HEX_COLOR,
+                d.ANNOUNCEMENT,
+                d.TYPE,
+                d.END_AT
+                /*함께하는 멤버 수*/
+            )
+                .from(d)
+                .leftJoin(dm).on(d.DIARY_NO.eq(dm.DIARY_NO))
+                .where(d.DELETED_AT.isNull())
+                .orderBy(sortField)
+                .limit(30)
+                .fetchInto(DiaryListDTO.class);
+
+    }
+
+    private SortField<?> getSortField(DiarySortOption option) {
+        Diaries d = Diaries.DIARIES.as("d");
+        SortField<?> sortField;
+        switch (option) {
+            case UPDATED_AT:
+                sortField = d.UPDATED_AT.desc();
+            case CREATED_AT:
+                sortField = d.CREATED_AT.desc();
+            case NAME:
+                sortField = d.NAME.asc();
+            case VIEW_CNT:
+                sortField = d.VIEW_CNT.desc();
+            default:
+                sortField = d.UPDATED_AT.desc();
+        }
+        return sortField;
+    }
 }
